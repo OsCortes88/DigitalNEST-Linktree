fcc70b444d58880f6ed2a95bdf94dd4096858102
OsCortes88 <oswaldoct2021@outlook.com>
2026-06-07 01:38:03 -0700

    fix description typos

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -5,8 +5,8 @@ function Carousel () {
 
   const carouselContent = [
     {label: "About Me", description: "As a young boy, STEM subjects were always my favorite. When I came across a CS course in High school, I instantly loved it. From there on out, I joined the CSin3 program to pursue a degree. As a graduate, I am now seeking new frontiers."},
-    {label: "My Philosophy", description: " I am a strong believer that things get done better when we work as a team. As a result, in any job I always look forward to brining my best and helping my team. After all, Rome wasn't built by one person."},
-    {label: "Hobbies", description: "Outside of work, you can usually find me hooping or playing soccer with my friends, jamming on my guitar to my favorite songs, or destroying my opponents in eFootball"},
+    {label: "My Philosophy", description: " I am a strong believer that things get done better when we work as a team. As a result, in any job I always look forward to bringing my best and helping my team. After all, Rome wasn't built by one person."},
+    {label: "Hobbies", description: "Outside of work, you can usually find me hooping or playing soccer with my friends, jamming on my guitar to my favorite songs, or destroying my opponents in eFootball."},
   ]
 
   const handlePrev = () => {
