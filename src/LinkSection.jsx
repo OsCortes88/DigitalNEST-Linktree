3e77726fa09fc3de7de8a27ee36308937b37b5d9
OsCortes88 <oswaldoct2021@outlook.com>
2026-06-07 01:41:44 -0700

    update porfolio link

--- src/LinkSection.jsx
+++ src/LinkSection.jsx
@@ -3,7 +3,7 @@ import LinkCard from "./LinkCard"
 function LinkSection() {
 
   const linkData = [
-    { icon: '/src/assets/portfolio.svg', label: 'Portfolio', reference: 'https://OsCortes88.github.io'},
+    { icon: '/src/assets/portfolio.svg', label: 'Portfolio', reference: 'https://oscortes88.github.io/projects.html'},
     { icon: '/src/assets/github.svg', label: 'Github', reference: 'https://github.com/OsCortes88' },
     { icon: '/src/assets/linkedin.svg', label: 'LinkedIn', reference: 'http://www.linkedin.com/in/oswaldo-cortes'},
     { icon: '/src/assets/document.svg', label: 'Resume', reference: 'https://docs.google.com/document/d/1ot_hIcSndYVe4vVYqQdZgg8NTJPuTOY_PhlAhl9MUIo/edit?usp=sharing'},
